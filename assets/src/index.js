e4adab31c32e0945e67014b55174dc67725b794c
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-21 15:17:07 +0100

    Card list implemented

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,14 @@
 const pageCards = document.querySelector(".page-cards");
 
+const cardList = [
+  { text: "Det här är den största dagen i Kims liv!" },
+  { text: "Han ska gå på sin första dejt någonsin!" },
+  { text: "Hon heter Julia." },
+  { text: "Det är lördag morgon och Kim ska förbereda sig för dejten…" },
+];
+
+let currentCardListIndex = 0;
+
 function createCard(text) {
   const card = document.createElement("div");
   card.className = "card swipe-card-in";
@@ -13,8 +22,21 @@ function createCard(text) {
 function onNextClick(event) {
   const parent = event.currentTarget.parentElement;
   parent.className = "card swipe-card-out";
+  onNextCard(parent);
+}
+
+function onNextCard(lastCard) {
+  currentCardListIndex++;
+  createCard(cardList[currentCardListIndex].text);
+  setTimeout(() => {
+    removeCard(lastCard);
+  }, 250);
+}
+
+function removeCard(card) {
+  card.remove();
 }
 
 (() => {
-  createCard("Det här är den största dagen i Kims liv!");
+  createCard(cardList[currentCardListIndex].text);
 })();
