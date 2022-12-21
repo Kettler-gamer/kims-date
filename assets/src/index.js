5558e57497c44652d422be97b9c8372739f76c2a
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-21 15:39:03 +0100

    a system that gets a new card list is implemented

--- assets/src/index.js
+++ assets/src/index.js
@@ -4,7 +4,10 @@ const cardList = [
   { text: "Det här är den största dagen i Kims liv!" },
   { text: "Han ska gå på sin första dejt någonsin!" },
   { text: "Hon heter Julia." },
-  { text: "Det är lördag morgon och Kim ska förbereda sig för dejten…" },
+  {
+    text: "Det är lördag morgon och Kim ska förbereda sig för dejten…",
+    newSrc: "test",
+  },
 ];
 
 let currentCardListIndex = 0;
@@ -26,11 +29,32 @@ function onNextClick(event) {
 }
 
 function onNextCard(lastCard) {
-  currentCardListIndex++;
-  createCard(cardList[currentCardListIndex].text);
+  if (cardList[currentCardListIndex].newSrc != undefined) {
+    const newList = fetch(
+      "assets/src/card-collection/" +
+        cardList[currentCardListIndex].newSrc +
+        ".json"
+    );
+    onGettingNewList(newList);
+  } else {
+    currentCardListIndex++;
+    createCard(cardList[currentCardListIndex].text);
+  }
   setTimeout(() => {
     removeCard(lastCard);
   }, 250);
+}
+
+async function onGettingNewList(newList) {
+  await Promise.all([newList]).then(async (response) => {
+    const list = await response[0].json();
+    cardList.splice(0, cardList.length);
+    for (let card of list) {
+      cardList.push(card);
+    }
+    currentCardListIndex = 0;
+    createCard(cardList[currentCardListIndex].text);
+  });
 }
 
 function removeCard(card) {
